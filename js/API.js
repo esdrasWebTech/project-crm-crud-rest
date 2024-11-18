7327e422cd537ed691fd7b83c93ef85ba09035f2
Esdras Ceballos Zurita <esdras.ceballosz@outlook.com>
2024-11-17 21:52:29 -0600

    Added reading and creating clients functions

---
+++ js/API.js
@@ -0,0 +1,34 @@
+const url = 'http://localhost:4000/clientes';
+
+// adding new client in DB
+export const addClient = async clientObj => {
+
+    try{
+        await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(clientObj),
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        })
+
+        window.location.href = 'index.html';
+
+    }catch(error){
+        console.log(error);
+    };
+
+};
+
+//get clients from DB
+export const getClients = async () =>{
+
+    try{
+        const response = await fetch(url);
+        const result = await response.json();
+        return result;
+
+    }catch(error){
+        console.log(error);
+    };
+};
